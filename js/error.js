d6a375472ce066eeba5041dff31dd5833d93af2f
luke.piotrak <48261254+luk-pio@users.noreply.github.com>
2023-11-16 14:24:59 -0500

    feat: add check for webcodec support

---
+++ js/error.js
@@ -0,0 +1,12 @@
+export function displayError(message) {
+    const errorContainer = document.getElementById("error-container")
+    errorContainer.innerText = message
+}
+
+export function errorHandler(event) {
+    displayError(event.message)
+}
+
+export function unhandledRejectionHandler(event) {
+    displayError(event.message)
+}
---
+++ js/index.js
@@ -0,0 +1,17 @@
+addEventListener("load", main);
+addEventListener("unhandledrejection", unhandledRejectionHandler);
+addEventListener("error", errorHandler);
+
+async function main() {
+  if (!checkWebcodecSupport()) {
+    displayError("Your browser must support WebCodecs to use this page. Please use one of the supported browsers https://caniuse.com/webcodecs");
+    return
+  }
+}
+
+function checkWebcodecSupport() {
+  if (self.isSecureContext === false) {
+    throw new Error('WebCodecs can only be used in secure contexts (HTTPS or localhost), https://developer.chrome.com/en/articles/webcodecs/#use')
+  }
+  return 'VideoEncoder' in window;
+}
